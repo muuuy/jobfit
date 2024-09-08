7b13c1d5844d127b57ae9c82f745cf329cc91bc0
Matthew Yu <myu2@umbc.edu>
2024-09-08 17:51:00 -0400

    feat: started project

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="">TEST</Button>
+      <h1 className="font-black">hello</h1>
+    </>
+  );
+}
+
+export default App;
